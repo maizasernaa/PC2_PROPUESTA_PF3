d5d55637fdecf0689669cfd97a535f15beab4e39
maizasernaa <102771046+maizasernaa@users.noreply.github.com>
2026-06-23 21:29:28 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,15 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Salubridad from './pages/Salubridad';
 
-// Este es el "Portero" que verifica si hay sesión
-function PrivateRoute({ children }: { children: JSX.Element }) {
-  const isAuth = localStorage.getItem('isAuthenticated');
-  return isAuth ? children : <Navigate to="/login" />;
-}
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        
-        {/* Aquí proteges las rutas */}
-        <Route path="/dashboard" element={
-          <PrivateRoute><Dashboard /></PrivateRoute>
-        } />
-        <Route path="/salubridad" element={
-          <PrivateRoute><Salubridad /></PrivateRoute>
-        } />
-        
-        {/* Redirección por defecto */}
-        <Route path="*" element={<Navigate to="/login" />} />
+        {/* Quitamos el PrivateRoute para que sea acceso libre */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/salubridad" element={<Salubridad />} />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
       </Routes>
     </BrowserRouter>
   );
